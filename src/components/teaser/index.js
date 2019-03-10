97ce7b175f0240a2ea5793d75f86092d2828d808
xiong-xiong <juha.stenroos@frantic.com>
2019-03-10 13:17:55 +0200

    add readme

--- src/components/teaser/index.js
+++ src/components/teaser/index.js
@@ -9,9 +9,9 @@ const Teaser = props => {
 
   return (
     <Link to={`/${props.prefix}/${props.post.slug}`} className="teaserBox cell large-6 medium-6 small-12">
-    <Heading heading={props.post.title} />
+    <Heading heading={props.post.title} /> 
    
-    {(props.cv === true) ? <Content content={props.post.content}  /> : null }
+    {props.cv? <Content content={props.post.content}  /> : null }
     {!props.cv ? <p><DateParser date={props.post.date} /></p> : <p style={{fontStyle: 'italic'}}>Last edited: <DateParser date={props.post.modified} /></p> }
     <span className="teaserLabel">{props.label}</span>
   </Link>
